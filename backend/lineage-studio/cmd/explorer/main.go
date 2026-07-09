f0c14e02aee8f5b4c94a705ffe6d54efd8ac1d83
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2026-07-09 07:49:50 +0630

    add lineage explorer program

---
+++ backend/lineage-studio/cmd/explorer/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"connectrpc.com/connect"
+	connectcors "connectrpc.com/cors"
+	"connectrpc.com/validate"
+
+	lse "github.com/SunMaungOo/lineage-studio/internal/lineage-explorer/connect"
+	"github.com/rs/cors"
+)
+
+func printHelp() {
+
+	fmt.Println("Arguments : <port> <repo location> <front end location>")
+	fmt.Println("<port> = port to run service on")
+	fmt.Println("<repo location> = folder where repo is saved")
+	fmt.Println("<front end location> =  folder where front end is located (must contain index.html)")
+
+}
+
+func main() {
+
+	args := os.Args[1:]
+
+	if len(args) != 3 {
+
+		printHelp()
+
+		return
+	}
+
+	serverPort, err := strconv.Atoi(args[0])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !(serverPort >= 0 && serverPort <= 65535) {
+		log.Fatal(fmt.Errorf("Invalid port number:%v", serverPort))
+	}
+
+	repoFolder := args[1]
+
+	frontEndLocation := filepath.Join(args[2], "index.html")
+
+	serverImplementation := LineageExplorerServer{
+		RepoLocation: repoFolder,
+		RemoteName:   "origin",
+	}
+
+	mux := http.NewServeMux()
+
+	path, handler := lse.NewLineageExplorerHandler(
+		&serverImplementation,
+		connect.WithInterceptors(validate.NewInterceptor()),
+	)
+
+	mux.Handle(path, handler)
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.ServeFile(writer, request, frontEndLocation)
+	})
+
+	protocol := new(http.Protocols)
+	protocol.SetHTTP1(true)
+	protocol.SetUnencryptedHTTP2(true)
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   connectcors.AllowedMethods(),
+		AllowedHeaders:   connectcors.AllowedHeaders(),
+		ExposedHeaders:   connectcors.ExposedHeaders(),
+		AllowCredentials: false,
+		MaxAge:           7200,
+	})
+
+	serverAddr := fmt.Sprintf("0.0.0.0:%v", serverPort)
+
+	server := http.Server{
+		Addr:      serverAddr,
+		Handler:   corsHandler.Handler(mux),
+		Protocols: protocol,
+	}
+
+	server.ListenAndServe()
+
+}
---
+++ backend/lineage-studio/cmd/explorer/server.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"context"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"connectrpc.com/connect"
+	"github.com/SunMaungOo/lineage-studio/internal/git"
+	lineage_explorer "github.com/SunMaungOo/lineage-studio/internal/lineage-explorer"
+	"github.com/SunMaungOo/lineage-studio/internal/repo"
+)
+
+const STATEMENT_SEPARATOR string = ";"
+
+const SOURCE_TARGET_SEPARATOR string = "->"
+
+const VALUE_SEPARATOR string = ","
+
+type LineageExplorerServer struct {
+	RepoLocation string
+	RemoteName   string
+}
+
+func (server LineageExplorerServer) getRepoSummary() ([]*lineage_explorer.RepoSummary, error) {
+
+	repos := []*lineage_explorer.RepoSummary{}
+
+	folderNames, err := repo.GetFolderNames(server.RepoLocation)
+
+	if err != nil {
+		return nil, nil
+	}
+
+	for _, folderName := range folderNames {
+
+		repoDirPath := filepath.Join(server.RepoLocation, folderName)
+
+		metadata, err := repo.LoadMetadata(repoDirPath)
+
+		if err != nil {
+			return nil, err
+		}
+
+		repos = append(repos, &lineage_explorer.RepoSummary{
+			Name:         folderName,
+			DatabaseType: string(metadata.Type),
+			Host:         metadata.Host,
+			DbName:       metadata.DB,
+		})
+
+	}
+
+	return repos, nil
+}
+
+func (server LineageExplorerServer) GetRepos(context context.Context, request *connect.Request[lineage_explorer.GetReposRequest]) (*connect.Response[lineage_explorer.GetReposResponse], error) {
+
+	summaries, err := server.getRepoSummary()
+
+	if err != nil {
+		return nil, nil
+	}
+
+	response := lineage_explorer.GetReposResponse{
+		Repos: summaries,
+	}
+
+	return connect.NewResponse(&response), nil
+}
+
+func (server LineageExplorerServer) getLineageDetail(initRepo *repo.Repo) []*lineage_explorer.LineageDetail {
+
+	lineageDetail := []*lineage_explorer.LineageDetail{}
+
+	for _, objectDetail := range repo.GetCurrentObjectInfos(initRepo) {
+
+		if len(strings.TrimSpace(objectDetail.Object.LineageHash)) == 0 {
+			continue
+		}
+
+		lineage := objectDetail.Object.Lineage
+
+		statements := strings.Split(lineage, STATEMENT_SEPARATOR)
+
+		lineageStatements := make([]*lineage_explorer.Statement, len(statements))
+
+		for index, statement := range statements {
+
+			cleanStatement := strings.TrimSpace(statement)
+
+			blocks := strings.Split(cleanStatement, SOURCE_TARGET_SEPARATOR)
+
+			sources := strings.Split(blocks[0], VALUE_SEPARATOR)
+
+			slices.Sort(sources)
+
+			uniqueSources := slices.Compact(sources)
+
+			var target string = ""
+
+			if len(blocks) > 1 {
+				target = blocks[1]
+			}
+
+			lineageStatements[index] = &lineage_explorer.Statement{
+				Sources: uniqueSources,
+				Target:  target,
+			}
+		}
+
+		lineageDetail = append(lineageDetail, &lineage_explorer.LineageDetail{
+			Repo:       initRepo.Name,
+			Name:       objectDetail.Object.Name,
+			Type:       string(objectDetail.Type),
+			Statements: lineageStatements,
+		})
+
+	}
+
+	return lineageDetail
+}
+
+func (server LineageExplorerServer) GetLineage(context context.Context, request *connect.Request[lineage_explorer.LineageRequest]) (*connect.Response[lineage_explorer.LineageResponse], error) {
+
+	repoLocation := server.RepoLocation
+
+	repoName := request.Msg.RepoName
+
+	remoteName := server.RemoteName
+
+	requestVersion := request.Msg.RepoVersion
+
+	responseVersion := requestVersion
+
+	repoDirPath := filepath.Join(repoLocation, repoName)
+
+	currentVersion, err := git.GitHeadCommit(repoDirPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	remoteVersion, err := git.GitRemoteHeadCommit(repoDirPath, remoteName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if currentVersion != remoteVersion {
+
+		err = git.GitPull(repoLocation)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	responseVersion, err = git.GitHeadCommit(repoDirPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if requestVersion == responseVersion {
+
+		return connect.NewResponse(&lineage_explorer.LineageResponse{
+			RepoVersion: responseVersion,
+			Status:      lineage_explorer.LineageStatus_LINEAGE_STATUS_NO_CHANGES,
+		}), nil
+
+	}
+
+	initRepo, err := repo.LoadRepo(repoLocation, repoName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	lineageDetail := server.getLineageDetail(&initRepo)
+
+	response := lineage_explorer.LineageResponse{
+		RepoVersion: responseVersion,
+		Details:     lineageDetail,
+		Status:      lineage_explorer.LineageStatus_LINEAGE_STATUS_OK,
+	}
+
+	return connect.NewResponse(&response), nil
+}
